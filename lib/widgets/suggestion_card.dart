af7db16fe2c90bb9b2e0c1b7dd030c667069cd7e
Gaby Nieva <gaby.nieva@outlook.com>
2023-08-09 09:51:04 -0600

    Re-design of the card

--- lib/widgets/suggestion_card.dart
+++ lib/widgets/suggestion_card.dart
@@ -5,30 +5,80 @@ class SuggestionCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-      margin: const EdgeInsets.all(15),
-      elevation: 10,
-      child: ClipRRect(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: const Image(
+              image: AssetImage('assets/images/sb57_kc_phi_120223.png'),
+              height: 350.0,
+              width: 350.0,
+            ),
+        ),
+        const SizedBox (width: 20.0,),
+        Column(
+          children: [
+            SizedBox(
+              width: 300,
+              height: 100,
+              child: Text(
+                'Super Bowl LVII', 
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+            ),
+            SizedBox(
+              width: 300,
+              height: 100,
+              child: Text("Después de una desventaja de doble dígito, los Chiefs regresan para arrebatarle el triunfo a Philadelphia y obtener su tercer Lombardi",
+                    style: Theme.of(context).textTheme.bodyMedium,
+                    textAlign: TextAlign.justify,
+              ),
+            ),
+          ],          
+        ),
+      ],
+    );
+
+    // return Card(
+    //   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    //   margin: const EdgeInsets.all(2.0),
+    //   elevation: 10.0,
+    //   color: Colors.grey,
+    //   child: ListTile(
+    //       leading:  const Image(
+    //           image: AssetImage('assets/images/sb57_kc_phi_120223.png'),
+    //           height: 350.0,
+    //           width: 350.0,
+    //         ),
+    //       title: Text("Super Bowl LVII", style: Theme.of(context).textTheme.headlineLarge,),
+    //       subtitle: Text("Después de una desventaja de doble dígito, los Chiefs regresan para arrebatarle el triunfo a Philadelphia y obtener su tercer Lombardi",
+    //                       style: Theme.of(context).textTheme.bodyMedium,
+    //       ),
+    //     ),
+    //   );
+
+      //ClipRRect(
 
         // Los bordes del contenido del card se cortan usando BorderRadius
-        borderRadius: BorderRadius.circular(30),
+        //borderRadius: BorderRadius.circular(30),
 
         // EL widget hijo que será recortado segun la propiedad anterior
-        child: Column(
+        /*child: Row(
           children: <Widget>[
             const Image(
               image: AssetImage('assets/images/sb57_kc_phi_120223.png'),
+              height: 350.0,
+              width: 350.0,
             ),
 
             // Usamos Container para el contenedor de la descripción
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: const Text('Super Bowl 57'),
-            ),
+            //Container(
+            //  padding: const EdgeInsets.all(3),
+            //  child: const Text('Super Bowl 57'),
+          //  ),
           ],
-        ),
-      ),
-    );
+        ),*/
+      //);
   }
 }
