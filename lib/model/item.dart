6452616672be56595deb507f5817e399260545f8
Gaby Nieva <gaby.nieva@outlook.com>
2023-08-11 00:05:03 -0600

    Adding model for items

---
+++ lib/model/item.dart
@@ -0,0 +1,21 @@
+class Item{
+
+  final String url;
+  final String title;
+  final String mini;
+  final String duration;
+  final String date;
+  final String description;
+  
+
+  const Item({required this.url, 
+              required this.title, 
+              required this.mini,
+              required this.duration,
+              required this.date,
+              required this.description
+               });
+
+  
+
+}
